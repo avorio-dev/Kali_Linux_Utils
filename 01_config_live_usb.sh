ed2eee58ff3af123dad7532e53e17fad82abaf40
Antonio Garofalo <avorio.dev@gmail.com>
2022-09-08 19:05:02 +0200

    Update and rename 01_config_live_usb to 01_config_live_usb.sh

---
+++ 01_config_live_usb.sh
@@ -0,0 +1,18 @@
+echo "-----------------------------------"
+echo "Init. Config: LIVE USB BASIC CONFIG"
+echo "-----------------------------------"
+
+echo ''
+echo "---------"
+echo ''
+
+echo "-> 01 - Setting QWERY layout keyboard.."
+sudo setxkbmap -layout it
+
+echo ''
+echo "---------"
+echo ''
+
+echo "-----------------"
+echo "CONFIG COMPLETED!"
+echo "-----------------"
