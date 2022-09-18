c02af4dd2768144a24859a17b2458905931c2a29
Antonio Garofalo <avorio.dev@gmail.com>
2022-09-18 10:39:12 +0200

    Update and rename arp_spoofing_bettercap.sh to arp_spoofing_bettercap.cap

--- arp_spoofing_bettercap.sh
+++
@@ -1,8 +0,0 @@
-echo "---------------------------------------------"
-echo "Init. Config: START SPOOFING SCRIPT BETTERCAP"
-echo "---------------------------------------------"
-
-interface='eth0'
-
-bettercap
-net.probe on
